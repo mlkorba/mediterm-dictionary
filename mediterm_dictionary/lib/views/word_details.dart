2d650a17b26a672312cd23b4c2c7b311ae5e631f
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-12-03 17:38:52 -0700

    designed word details page

--- mediterm_dictionary/lib/views/word_details.dart
+++ mediterm_dictionary/lib/views/word_details.dart
@@ -18,15 +18,110 @@ class WordDetailPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text(
-              'Definition:',
+              'ID:',
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
+            Text(definition.id),
             const SizedBox(height: 8),
-            Text(definition.definition),
+
+            const Text(
+              'Stems:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              definition.stems.join(', '), // Join stems without square brackets
+            ),
+
+            const SizedBox(height: 8),
+
+            const Text(
+              'HWI:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            // Display 'hwi' using a separate widget
+            HwiWidget(hwi: definition.hwi),
+
+            const SizedBox(height: 8),
+
+            const Text(
+              'FL:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(definition.fl),
+
+            const SizedBox(height: 8),
+
+            // Display 'DEF' using a separate widget
+            DefinitionWidget(def: definition.def),
+
+            if (definition.def.isNotEmpty &&
+                definition.def[0]['cats'] != null) ...[
+              const SizedBox(height: 8),
+              const Text(
+                'cat:',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(cleanUpDefinition(definition.def[0]['cats'].toString())),
+            ],
+
+            // Remove 'sound' references from here
+
             // Add more details as needed
           ],
         ),
       ),
     );
+  }
+
+  String cleanUpDefinition(String input) {
+    // Replace unwanted symbols or text here
+    return input.replaceAll(RegExp(r'\{bc\}'), ''); // Remove {bc}
+  }
+}
+
+class HwiWidget extends StatelessWidget {
+  final Map<String, dynamic> hwi;
+
+  const HwiWidget({Key? key, required this.hwi}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('hw: ${hwi['hw']}'),
+        // Add additional widgets or formatting as needed for 'hwi'
+      ],
+    );
+  }
+}
+
+class DefinitionWidget extends StatelessWidget {
+  final List<Map<String, dynamic>> def;
+
+  const DefinitionWidget({Key? key, required this.def}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'DEF:',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        for (var definition in def) ...[
+          Text(cleanUpDefinition(definition.toString())),
+        ],
+      ],
+    );
+  }
+
+  String cleanUpDefinition(String input) {
+    // Extract text after {bc} and remove leading and trailing whitespaces
+    final result =
+        RegExp(r'\{bc\}(.+?)\]').firstMatch(input)?.group(1)?.trim() ?? '';
+
+    return result;
   }
 }
