b9321fa6dc620589e9772ad9abce850171f9abbb
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-12-12 19:11:19 -0700

    designed top part of screen

--- mediterm_dictionary/lib/views/word_details.dart
+++ mediterm_dictionary/lib/views/word_details.dart
@@ -10,72 +10,98 @@ class WordDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(definition.term),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'ID:',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(definition.id),
-            const SizedBox(height: 8),
-
-            const Text(
-              'Stems:',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              definition.stems.join(', '), // Join stems without square brackets
-            ),
-
-            const SizedBox(height: 8),
-
-            const Text(
-              'HWI:',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            // Display 'hwi' using a separate widget
-            HwiWidget(hwi: definition.hwi),
-
-            const SizedBox(height: 8),
-
-            const Text(
-              'FL:',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(definition.fl),
-
-            const SizedBox(height: 8),
-
-            // Display 'DEF' using a separate widget
-            DefinitionWidget(def: definition.def),
-
-            if (definition.def.isNotEmpty &&
-                definition.def[0]['cats'] != null) ...[
-              const SizedBox(height: 8),
-              const Text(
-                'cat:',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Text(cleanUpDefinition(definition.def[0]['cats'].toString())),
-            ],
-
-            // Remove 'sound' references from here
-
-            // Add more details as needed
-          ],
+        backgroundColor: Colors.red,
+        title: const Text(
+          'MediTerm Dictionary',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 16,
+          ),
         ),
+        centerTitle: true,
+        leading: BackButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          color: Colors.white,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20.0),
+              ),
+            ),
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: Text(
+                capitalize(definition.id),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Stems:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  definition.stems
+                      .join(', '), // Join stems without square brackets
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  'HWI:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                HwiWidget(hwi: definition.hwi),
+                const SizedBox(height: 8),
+                const Text(
+                  'FL:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(definition.fl),
+                const SizedBox(height: 8),
+                DefinitionWidget(def: definition.def),
+                if (definition.def.isNotEmpty &&
+                    definition.def[0]['cats'] != null) ...[
+                  const SizedBox(height: 8),
+                  const Text(
+                    'cat:',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(cleanUpDefinition(definition.def[0]['cats'].toString())),
+                ],
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
   String cleanUpDefinition(String input) {
-    // Replace unwanted symbols or text here
     return input.replaceAll(RegExp(r'\{bc\}'), ''); // Remove {bc}
+  }
+
+  String capitalize(String input) {
+    if (input.isEmpty) {
+      return input;
+    }
+    return input[0].toUpperCase() + input.substring(1);
   }
 }
 
@@ -89,10 +115,16 @@ class HwiWidget extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text('hw: ${hwi['hw']}'),
-        // Add additional widgets or formatting as needed for 'hwi'
+        Text('hw: ${capitalize(hwi['hw'])}'), // Capitalize the first word in hw
       ],
     );
+  }
+
+  String capitalize(String input) {
+    if (input.isEmpty) {
+      return input;
+    }
+    return input[0].toUpperCase() + input.substring(1);
   }
 }
 
@@ -118,10 +150,8 @@ class DefinitionWidget extends StatelessWidget {
   }
 
   String cleanUpDefinition(String input) {
-    // Extract text after {bc} and remove leading and trailing whitespaces
     final result =
         RegExp(r'\{bc\}(.+?)\]').firstMatch(input)?.group(1)?.trim() ?? '';
-
     return result;
   }
 }
