2924a948bf2916cf4168e3dcb1246a8da5fb3c71
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-12-14 00:13:42 -0700

    implemented FirebaseAuth

--- mediterm_dictionary/lib/main.dart
+++ mediterm_dictionary/lib/main.dart
@@ -1,12 +1,23 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'views/home.dart';
-import 'views/medical_dictionary.dart';
-import 'services/api_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:mediterm_dictionary/views/login_screen.dart';
+import 'package:mediterm_dictionary/views/medical_dictionary.dart';
+import 'package:mediterm_dictionary/services/api_service.dart';
 
 final ApiService apiService =
     ApiService('e685bd9f-83a4-429f-bfa1-599dfb152bec');
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: 'AIzaSyDEJuZeSWLTPNj-8mqd-ELQ1w73GPG6XcM',
+              appId: '1:921686556422:android:4afe551619c1c174806887',
+              messagingSenderId: '921686556422',
+              projectId: 'mediterm-dictionary'))
+      : await Firebase.initializeApp();
   runApp(const MainApp());
 }
 
