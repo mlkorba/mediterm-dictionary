630a405c89b7488c5b4d0d1016f00dba713a574d
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-12-14 00:58:55 -0700

    designed layout and theme

--- mediterm_dictionary/lib/views/bookmark_list.dart
+++ mediterm_dictionary/lib/views/bookmark_list.dart
@@ -65,49 +65,86 @@ class _BookmarkListPageState extends State<BookmarkListPage> {
           color: Colors.white,
         ),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Container(
-            color: Colors.red,
-            padding: const EdgeInsets.all(16.0),
-            child: const Center(
-              child: Text(
-                'Bookmark List',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 32,
+          Column(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(
+                        20.0), // Adjust this value for the bottom left border radius
+                  ),
                 ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: widget.bookmarkedTerms.length,
-              itemBuilder: (context, index) {
-                final term = widget.bookmarkedTerms[index];
-                return Card(
-                  elevation: 4.0,
-                  margin: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    title: Text(
-                        _capitalizeFirstLetter(term.term.replaceAll('*', ''))),
-                    // subtitle: Text(term.definition),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.bookmark),
-                      onPressed: () {
-                        setState(() {
-                          widget.bookmarkedTerms.remove(term);
-                        });
-
-                        // Show the undo snackbar and pass the updated list back to MedicalDictionary
-                        _showUndoSnackBar(term);
-                        widget.onUnbookmark(term);
-                      },
+                padding: const EdgeInsets.all(16.0),
+                child: const Center(
+                  child: Text(
+                    'Bookmark List',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 32,
                     ),
                   ),
-                );
-              },
+                ),
+              ),
+              Container(
+                height: 25,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [Colors.white, Colors.red],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top:
+                78, // Adjust this value to position the Expanded widget as desired
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(
+                      20.0), // Adjust this value for the top-right corner radius
+                  topLeft: Radius.circular(
+                      20.0), // Adjust this value for the top-right corner radius
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                  itemCount: widget.bookmarkedTerms.length,
+                  itemBuilder: (context, index) {
+                    final term = widget.bookmarkedTerms[index];
+                    return Card(
+                      elevation: 4.0,
+                      margin: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        title: Text(
+                          _capitalizeFirstLetter(term.term.replaceAll('*', '')),
+                        ),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.bookmark),
+                          onPressed: () {
+                            setState(() {
+                              widget.bookmarkedTerms.remove(term);
+                            });
+                            _showUndoSnackBar(term);
+                            widget.onUnbookmark(term);
+                          },
+                        ),
+                      ),
+                    );
+                  }, // itemBuilder
+                ),
+              ),
             ),
           ),
         ],
