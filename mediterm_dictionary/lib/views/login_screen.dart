01c8b26681c90e52a588298dd98b49202fde22a2
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-12-14 00:12:00 -0700

    added signUpOption method

--- mediterm_dictionary/lib/views/login_screen.dart
+++ mediterm_dictionary/lib/views/login_screen.dart
@@ -84,3 +84,26 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have an account?",
+            style: TextStyle(
+              color: Colors.white,
+            )),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const SignUpScreen()));
+          },
+          child: const Text(
+            " Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+}
